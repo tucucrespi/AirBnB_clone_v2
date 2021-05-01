09efe0da09cf1b7e10e115faa213700c27fde241
tucucrespi <tucucrespi@hotmail.com>
2021-05-01 22:59:22 +0000

    add task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,10 +4,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-def first_task()
+def first_task():
     """ Return Hello HBNB """
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
